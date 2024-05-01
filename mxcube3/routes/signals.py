40f992651d2d1771b80e9df2e2dab491d07b647c
ssrfmx <qinwenming@sari.ac.cn>
2024-05-01 13:34:24 +0800

    20240501

--- mxcube3/routes/signals.py
+++ mxcube3/routes/signals.py
@@ -90,9 +90,9 @@ def handle_auto_mount_next(entry):
 
 def diffractometer_phase_changed(*args):
     data = {"msg": "Diffractometer phase changed", "phase": args}
-    # logging.getLogger("HWR").info(
-    #     "==== Diffractometer phase changed to %s" % args
-    # )
+    logging.getLogger("user_level_log").info(
+        "Diffractometer phase changed to %s" % args
+    )
     server.emit("diff_phase_changed", data, namespace="/hwr")
 
 
